c39001a7ce68da5b528b871816570236b29e2562
Alexander Schuch <alexander@schuch.me>
2016-01-09 21:10:17 +0100

    fix watchOS and tvOS frameworks with carthage

---
+++ ExampleWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ExampleWatch Extension
+//
+//  Created by Alexander Schuch on 09/01/16.
+//  Copyright © 2016 Alexander Schuch. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import Argonaut
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
